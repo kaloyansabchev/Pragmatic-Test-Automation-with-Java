2af44f9bfd2f03068514ab815c7bd0c82ff203b8
Kaloyan <kaloyanboichev@abv.bg>
2021-03-11 13:40:06 +0200

    fixing the task five

--- Homework - Lecture 1/TaskFive.java
+++ Homework - Lecture 1/TaskFive.java
@@ -14,20 +14,26 @@ public class TaskFive {
 
         if (num1 > num2 && num1 > num3){
             if (num2 > num3){
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num1 + ", " + num2 + ", " + num3);
             }else {
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num1 + ", " + num3 + ", " + num2);
             }
         } else if (num2 > num1 && num2 > num3){
             if (num1 > num3){
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num2 + ", " + num1 + ", " + num3);
             }else {
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num2 + ", " + num3 + ", " + num1);
             }
-        } else {
-            if (num2 > num3){
+        } else if (num3 > num1 && num3 > num2){
+            if (num2 > num1){
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num3 + ", " + num2 + ", " + num1);
             }else {
+                System.out.println("The order of the numbers in descending order is:");
                 System.out.println(num3 + ", " + num1 + ", " + num2);
             }
         }
