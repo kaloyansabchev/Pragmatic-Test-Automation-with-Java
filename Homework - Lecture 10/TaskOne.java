d96b419605fcd8386f72502aa8e25d352d3e4335
Kaloyan <kaloyanboichev@abv.bg>
2021-04-13 15:34:32 +0300

    fixing task 1and2 from homework lectue 10

--- Homework - Lecture 10/TaskOne.java
+++ Homework - Lecture 10/TaskOne.java
@@ -11,25 +11,25 @@ public class TaskOne {
         List<Integer> arrayListOfNumbers = new ArrayList<>();
 
         for (int i = 1; i < NUMBERS_IN_LIST; i++) {
-            System.out.println("Please enter the " + i + " ");
+            System.out.println("Please enter " + i + "-number: ");
             int n = Integer.parseInt(sc.nextLine());
             arrayListOfNumbers.add(n);
         }
         System.out.println("The result ArrayList is: " + arrayListOfNumbers + "\n");
 
-        Iterator itr = arrayListOfNumbers.iterator();
+        Iterator<Integer> itr = arrayListOfNumbers.iterator();
         int min = arrayListOfNumbers.get(0);
         boolean hasNumberMultipleBy3 = false;
 
-        while(itr.hasNext()) {
-            int tmpInt = (int)itr.next();
-            if(tmpInt % 3 == 0 && (min % 3 != 0 || tmpInt < min)){
+        while (itr.hasNext()) {
+            int tmpInt = (int) itr.next();
+            if (tmpInt % 3 == 0 && (min % 3 != 0 || tmpInt < min)) {
                 hasNumberMultipleBy3 = true;
                 min = tmpInt;
             }
         }
 
-        if(!hasNumberMultipleBy3 && (min % 3 != 0)) {
+        if (!hasNumberMultipleBy3 && (min % 3 != 0)) {
             System.out.println("There are no number multiple by 3 in the ArrayList.");
         } else {
             System.out.println("The minimum number multiple by 3 is " + min);
