3d9beabd3a96c7d1702a848f33951848090e4ca6
kaloyansabchev <77458843+kaloyansabchev@users.noreply.github.com>
2021-05-15 17:11:19 +0300

    finished the homework 16

---
+++ Homework - Lecture 16/src/test/java/CheckboxTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class CheckboxTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void startUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kallo\\Desktop\\Pragmatic - March\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://pragmatic.bg/automation/lecture13/Config.html");
+    }
+    @Test
+    public void test() {
+        WebElement airBags = this.driver.findElement(By.xpath("//*[@id=\"tabs-1\"]/p[3]/input[2]"));
+        WebElement parkingSensor = this.driver.findElement(By.xpath("//*[@id=\"tabs-1\"]/p[3]/input[3]"));
+
+        Actions builder = new Actions(driver);
+
+        builder
+            .click(airBags)
+            .click(parkingSensor)
+            .perform();
+
+        boolean isBagsAreSelected = airBags.isSelected();
+        boolean isSensorsAreSelected = airBags.isSelected();
+        Assert.assertTrue(isBagsAreSelected);
+        Assert.assertTrue(isSensorsAreSelected);
+
+    }
+
+    private void click(WebElement parkingSensor) {
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- Homework - Lecture 16/src/test/java/DropDownTest.java
+++ Homework - Lecture 16/src/test/java/DropDownTest.java
@@ -1,46 +1,34 @@
 import org.openqa.selenium.WebDriver;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class DropDownTest {
 
     WebDriver driver;
 
     @BeforeTest
-    public void startUp (){
+    public void startUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\kallo\\Desktop\\Pragmatic - March\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://shop.pragmatic.bg/admin");
     }
 
-    @AfterTest
-    public void tearDown(){
-        driver.quit();
-    }
-
     @Test
-    public void test() throws InterruptedException {
-        driver.findElement(By.xpath("//input[starts-with(@name,'username')]")).sendKeys("admin");
-        driver.findElement(By.xpath("//input[starts-with(@name,'password')]")).sendKeys("parola123!");
+    public void test() {
+        driver.findElement(By.xpath("//*[@id=\"input-username\"]")).sendKeys("admin");
+        driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("parola123!");
         driver.findElement(By.cssSelector(".btn-primary")).click();
 
         driver.findElement(By.id("menu-sale")).click();
         driver.findElement(By.linkText("Orders")).click();
 
-        Select orderStatusDropDown = new Select(driver.findElement(By.id("input-order-status")));
-
-        String[] expectedValues = {
+        String[] expectedOrderStatus = {
                 "",
                 "Missing Orders",
                 "Canceled",
@@ -56,18 +44,23 @@ public class DropDownTest {
                 "Refunded",
                 "Reversed",
                 "Shipped",
-                "Voided" };
+                "Voided"};
 
+        Select actualOrderStatus = new Select(driver.findElement(By.name("filter_order_status_id")));
         List<String> actualValues = new ArrayList<>();
-
-        List<WebElement> allDropdownElements = orderStatusDropDown.getOptions();
+        List<WebElement> allDropdownElements = actualOrderStatus.getOptions();
 
         for (WebElement curDropdownElement : allDropdownElements) {
             actualValues.add(curDropdownElement.getText());
         }
 
-        Assert.assertEquals(expectedValues, actualValues.toArray());
+        Assert.assertEquals(expectedOrderStatus, actualValues.toArray());
 
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
     }
 
 }
