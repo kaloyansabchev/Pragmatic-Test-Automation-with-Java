0be4668b978d2dab378ecb2f88fcacc4c66ae3b5
Kaloyan <kaloyanboichev@abv.bg>
2021-05-19 14:17:35 +0300

    addint the start of homework 18

---
+++ Homework - Lecture 18/src/main/java/utils/Browser.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Browser {
+
+    public static WebDriver driver;
+
+    public static void open() {
+        System.setProperty("webdriver.chrome.driver", "E:\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+
+
+    public static void quit() {
+        driver.quit();
+    }
+}
